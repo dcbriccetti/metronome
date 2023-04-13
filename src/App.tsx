97099e32fde8edf0fbf0eff60c7f04fa313b856a
Dave Briccetti <daveb@davebsoft.com>
2023-04-13 11:34:53 -0700

    Inline code from metronome-app.tsx. Move styles to App.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import MetronomeApp from "./metronome-app"
 import {VisSettings, Visualization} from "./components/Visualization"
 import Tempo from './components/Tempo'
 import SoundType from "./components/SoundType"
 import VisType from "./components/VisType"
 import MetronomeSound, {Listener} from "./metronome-sound"
 import StartStopButton from "./components/StartStopButton"
+import "./App.css"
 
 export default function App(): JSX.Element {
     const visSettings: VisSettings = {
@@ -21,21 +21,20 @@ export default function App(): JSX.Element {
         setStartTime: (startTime: number) => visSettings.startTime = startTime
     };
     const metroSound = new MetronomeSound('audio/', soundFilenames, metroSoundListener);
-    const metronomeApp = new MetronomeApp(metroSound, visSettings);
 
     return <div className="container">
         <h1>Metronome</h1>
-        <p style={{fontSize: '70%'}}>By <a href="https://davebsoft.com">Dave Briccetti</a>
-            <a style={{marginLeft: '1em'}} href="https://github.com/dcbriccetti/metronome">Source code</a></p>
+        <p className='byline'>By <a href="https://davebsoft.com">Dave Briccetti</a>
+            <a className='source-link' href="https://github.com/dcbriccetti/metronome">Source code</a></p>
         <div className="row">
             <div className="col-sm">
                 <Tempo tempoBpm={visSettings.tempoBpm}
-                       onChange={tempo => metronomeApp.setTempo(tempo)}/>
+                       onChange={tempo => metroSound.setTempo(tempo)}/>
                 <SoundType filenames={soundFilenames}
-                           onChange={index => metronomeApp.setSound(index + 1)}/>
+                           onChange={index => metroSound.setSound(index + 1)}/>
                 <VisType names={visSettings.names}
-                         onChange={(index: number) => metronomeApp.setVisualization(index)}/>
-                <StartStopButton metroSound={metroSound} onClick={() => metronomeApp.toggle()}/>
+                         onChange={(index: number) => visSettings.visualizationType = index}/>
+                <StartStopButton metroSound={metroSound} onClick={() => metroSound.toggle()}/>
             </div>
             <div className="col-sm" id='visualization'>
                 <Visualization getTime={() => metroSound.audioContext.currentTime} visSettings={visSettings}/>
