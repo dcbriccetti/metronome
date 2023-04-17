a6d9a84780ea0c027fec9bacad2f1aa486fad060
Dave Briccetti <daveb@davebsoft.com>
2023-04-16 22:22:11 -0700

    Add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import VisType from "./components/VisType"
 import MetronomeSound from "./metronome-sound"
 import StartStopButton from "./components/StartStopButton"
 import "./App.css"
+import {Col, Container, Row} from "react-bootstrap"
 
 export default function App(): JSX.Element {
     const soundFilenames = ['High_Woodblock.wav', 'Low_Woodblock.wav', 'High_Bongo.wav',
@@ -13,33 +14,35 @@ export default function App(): JSX.Element {
     let tempoBpm = 60
     let visualizationType = 1
     const sound = new MetronomeSound('audio/', soundFilenames, {
-        setTempo:     (t: number) => tempoBpm = t,
+        setTempo: (t: number) => tempoBpm = t,
         setStartTime: (s: number) => startTime = s
     })
 
-    return <div className="container">
-        <h1>Metronome</h1>
-        <p className='byline'>By <a href="https://davebsoft.com">Dave Briccetti</a>
-            <a className='source-link' href="https://github.com/dcbriccetti/metronome">Source code</a></p>
-        <div className="row">
-            <div className="col-sm">
-                <Tempo tempoBpm={tempoBpm}
-                       onChange={(t: number) => {
-                           tempoBpm = t
-                           sound.setTempo(t)
-                       }}/>
-                <SoundType filenames={soundFilenames}
-                           onChange={index => sound.setSound(index + 1)}/>
-                <VisType names={['Spinning Circle', 'Circle']}
-                         onChange={(index: number) => visualizationType = index}/>
-                <StartStopButton sound={sound} />
-            </div>
-            <div className="col-sm" id="visualization">
-                <Visualization getTime={() => sound.audioContext.currentTime}
-                               getTempoBpm={() => tempoBpm}
-                               getStartTime={() => startTime}
-                               getVisualizationType={() => visualizationType}/>
-            </div>
-        </div>
-    </div>
+    return (
+        <Container>
+            <h1>Metronome</h1>
+            <p className="byline">By <a href="https://davebsoft.com">Dave Briccetti</a>
+                <a className="source-link" href="https://github.com/dcbriccetti/metronome">Source code</a></p>
+            <Row>
+                <Col>
+                    <Tempo tempoBpm={tempoBpm}
+                           onChange={(t: number) => {
+                               tempoBpm = t
+                               sound.setTempo(t)
+                           }}/>
+                    <SoundType filenames={soundFilenames}
+                               onChange={index => sound.setSound(index + 1)}/>
+                    <VisType names={['Spinning Circle', 'Circle']}
+                             onChange={(index: number) => visualizationType = index}/>
+                    <StartStopButton sound={sound}/>
+                </Col>
+                <Col id="visualization">
+                    <Visualization getTime={() => sound.audioContext.currentTime}
+                                   getTempoBpm={() => tempoBpm}
+                                   getStartTime={() => startTime}
+                                   getVisualizationType={() => visualizationType}/>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
